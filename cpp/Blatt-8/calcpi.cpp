90697e985c33612b120e88ec9cadf21d8ad57ae9
Lasse W <garfield.pruin@pruin.org>
2018-07-02 17:54:15 +0200

    added blatt 9

--- cpp/Blatt-8/calcpi.cpp
+++ cpp/Blatt-8/calcpi.cpp
@@ -8,19 +8,25 @@
 
 void calcpi(const long long N, long long &hits, std::mutex &mutex, const int seed)
 {
+	std::default_random_engine generator(seed);
+	std::uniform_real_distribution<double> distribution(0,1.0);
 	// fixme
+	// std::mutex m;
 	for (long long i = 0; i < N; ++i)
 	{
-		double x = 0; // fixme
-		double y = 0; // fixme
+		double x = distribution(generator); // fixme
+		double y = distribution(generator); // fixme
 		if (std::sqrt(x*x + y*y) <= 1){
+			mutex.lock();
 			++hits;
+			mutex.unlock();
 		}
 	}
 }
 
 int main(int argc, char **argv)
 {
+	
 	std::mutex mutex;
 	const long long N = 1e7;
 	long long hits = 0;
@@ -33,6 +39,9 @@ int main(int argc, char **argv)
 	for (int i = 0; i < num_threads; ++i) {
 		threads.push_back(std::thread(calcpi, N / num_threads, std::ref(hits), std::ref(mutex), i + 100));
 	}
+	
+	for (std::thread & t : threads)
+		t.join();
 	// fixme
 
 	const double V = 2*2;
